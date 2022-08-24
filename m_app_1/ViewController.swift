af2b17ab08f0d11d2232363ddb28820b37a9b623
ollenaaa <elenasazonets@gmail.com>
2022-08-24 07:55:08 +0300

    Initial Commit

---
+++ m_app_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  m_app_1
+//
+//  Created by Lena on 24.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
